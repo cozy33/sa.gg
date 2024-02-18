767cc25e614707649e29bae0665814eeeffe01b8
stayforyou <bread.s@kakao.com>
2024-02-18 10:24:54 +0900

    ktlint 적용, 프로젝트 생성

---
+++ src/main/kotlin/me/bread/supplybase/SupplyBaseApplication.kt
@@ -0,0 +1,11 @@
+package me.bread.supplybase
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SupplyBaseApplication
+
+fun main(args: Array<String>) {
+    runApplication<SupplyBaseApplication>(*args)
+}
---
+++ src/test/kotlin/me/bread/supplybase/SupplyBaseApplicationTests.kt
@@ -0,0 +1,11 @@
+package me.bread.supplybase
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SupplyBaseApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
